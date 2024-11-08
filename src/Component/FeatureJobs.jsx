52e92361d5bde9b435442d74b964d2176086060c
AsifurRahman10 <1720105@iub.edu.bd>
2024-11-08 14:58:21 +0600

    display all job btn function added

--- src/Component/FeatureJobs.jsx
+++ src/Component/FeatureJobs.jsx
@@ -1,10 +1,20 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Title } from "./Title";
 import { JobDetailsData } from "../Context/JobDetailsProvider";
 import { JobCart } from "./JobCart";
 
 export const FeatureJobs = () => {
   const { jobDetails } = useContext(JobDetailsData);
+  const [displayJOb, setDisplayJOb] = useState([]);
+  useEffect(() => {
+    const defaultJobs = jobDetails.slice(0, 4);
+    setDisplayJOb(defaultJobs);
+  }, [jobDetails]);
+
+  const handleSeeAllJobs = () => {
+    setDisplayJOb(jobDetails);
+  };
+  console.log(displayJOb);
   return (
     <div className="pb-24 w-11/12 md:w-9/12 mx-auto">
       <Title
@@ -12,12 +22,15 @@ export const FeatureJobs = () => {
         details="Explore thousands of job opportunities with all the information you need. Its your future"
       ></Title>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-12 mt-8">
-        {jobDetails.map((item, index) => (
+        {displayJOb.map((item, index) => (
           <JobCart key={index} item={item} />
         ))}
       </div>
       <div className="text-center">
-        <button className="bg-hero-pattern btn px-8 text-xl text-white font-extrabold mt-10 h-[60px]">
+        <button
+          onClick={handleSeeAllJobs}
+          className="bg-hero-pattern btn px-8 text-xl text-white font-extrabold mt-10 h-[60px]"
+        >
           See All Jobs
         </button>
       </div>
